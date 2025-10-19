eef41ac2156a6cacc829495386cad1ac8d45892a
samstha07 <samshrestha85@gmail.com>
2025-10-19 17:13:10 +0545

    some changes

--- src/app/[locale]/payment/success/page.tsx
+++ src/app/[locale]/payment/success/page.tsx
@@ -1,8 +1,5 @@
 import PaymentSuccessView from '@/modules/payment/ui/views/payment-success-view';
 
-// Force dynamic rendering - don't prerender this page at build time
-export const dynamic = 'force-dynamic';
-
 export default function SuccessPage() {
   return <PaymentSuccessView />;
 }
