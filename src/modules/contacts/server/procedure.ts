62e506f1df2f833886b71f12a3fbe8a0d3dcd97e
samstha07 <samshrestha85@gmail.com>
2025-10-20 18:51:48 +0545

    feat: removed sanity write token

--- src/modules/contacts/server/procedure.ts
+++ src/modules/contacts/server/procedure.ts
@@ -1,4 +1,4 @@
-import { client, writeClient } from '@/sanity/lib/client';
+import { client } from '@/sanity/lib/client';
 import { ContactPage } from '@/sanity/types';
 import { createTRPCRouter, publicProcedure } from '@/trpc/init';
 import { TRPCError } from '@trpc/server';
@@ -24,7 +24,7 @@ export const contactRouter = createTRPCRouter({
     .input(contactFormSchema)
     .mutation(async ({ input }) => {
       try {
-        const result = await writeClient.create({
+        const result = await client.create({
           _type: 'contact',
           fullName: input.fullName,
           email: input.email,
