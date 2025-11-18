cfc9b800ff861b454a750fb7b4d855d244adb079
samstha07 <samshrestha85@gmail.com>
2025-11-18 18:52:47 +0545

    fix: build error

--- src/modules/about/ui/views/about-view.tsx
+++ src/modules/about/ui/views/about-view.tsx
@@ -62,7 +62,6 @@ function AboutViewSuspense() {
         />
       )} */}
       {/* <WhoDoWeHelpSection /> */}
-      {/* <AboutUsTeamSection /> */}
 
       {data?.partnersSection && (
         <PartnersSection
