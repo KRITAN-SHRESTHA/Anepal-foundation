9b23ff65c115698a623dc9609e20db27fa77c8ec
samstha07 <samshrestha85@gmail.com>
2025-11-20 19:09:16 +0545

    feat: fix sanity build error

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,13 +1,15 @@
+import { env } from '@/env';
+
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-06-05';
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 );
 
